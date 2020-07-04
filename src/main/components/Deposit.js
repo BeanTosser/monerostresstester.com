ec2756f408e5a8e2e6209a03544c3406d7af7c91
timetherewere <mikjosjon@gmail.com>
2020-07-04 19:22:06 -0400

    Remove space from "Sign Out.js" file name.

--- src/main/components/Deposit.js
+++ src/main/components/Deposit.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function Home(){
+export default function Deposit(){
   return(
     <>
-      <h1>Home</h1>
+      <h1>Deposit</h1>
     </>
   );
 }
