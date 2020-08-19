ec2f574b304560db0677d5b3bfe90beaec40185c
timetherewere <mikjosjon@gmail.com>
2020-08-19 08:04:43 -0400

    Progress on main wallet page

--- src/main/components/pages/Wallet.js
+++ src/main/components/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import {Page_Box} from "../Widgets.js";
+import "./wallet.css";
 export default function Wallet(props){
   return(
-    <>
-      <h1>Main wallet page (placeholder)</h1>
-    </>
+    <Page_Box>
+       <Wallet_Page_Section />
+    </Page_Box>
+  );
+}
+
+function Wallet_Page_Section(props){
+  return(
+    <div className="wallet_page_sections_container">
+      <div className="wallet_page_section">
+        <div className="wallet_page_section_text">
+          <div className="wallet_page_section_label">
+            Test Label
+          </div>
+          <div className="wallet_page_section_value">
+            Test Value
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
   );
 }
