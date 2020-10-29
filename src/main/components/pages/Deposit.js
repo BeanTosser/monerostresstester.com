e42088fce715b92564293b69ce14fdf65a3c1c30
timetherewere <mikjosjon@gmail.com>
2020-10-29 12:43:30 -0400

    Deposit page styling in progress

--- src/main/components/pages/Deposit.js
+++ src/main/components/pages/Deposit.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./deposit.css";
+
 import {Page_Box} from "../Widgets.js";
 
 export default function Deposit(props){
   return(
     <Page_Box className="deposit_page_box">
-      <h1>Deposit</h1>
-      <div>{props.depositQrCode}</div>
-      <div>{props.walletAddress}</div>
+      <div className="title"><h1>Deposit</h1></div>
+      <div className="code_image">{props.depositQrCode}</div>
+      <div className="code_text">{props.walletAddress}</div>
     </Page_Box>
   );
 }
